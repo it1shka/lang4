5e9fc77976e64bf11d9a24cbdbd1190156cd9f62
it1shka <2tishbel@gmail.com>
2020-09-23 12:15:59 +0300

    yeeeeeeeeeeee

--- runner.js
+++ runner.js
@@ -1,5 +1,14 @@
 const codeArea = document.getElementById('codeArea');
 
+codeArea.onkeydown = function(e){
+    if(e.keyCode==9 || e.which==9){
+        e.preventDefault();
+        let s = this.selectionStart;
+        this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+        this.selectionEnd = s+1; 
+    }
+}
+
 function runCode(){
     const source = codeArea.value;
     run(source);
